dd20ee96dcc2faab4a14de3cab0e626f8bf7c061
Juncai <cai.jun@husky.neu.edu>
2015-12-06 20:07:43 -0500

    bug fix

--- Authentication.py
+++ Authentication.py
@@ -13,14 +13,13 @@ class Authentication():
         3 : auth success
     """
 
-    def __init__(self, addr, p, g, cryto_service):
+    def __init__(self, addr, cryto_service):
         assert isinstance(cryto_service, CryptoService)
         self.cryto_service = cryto_service
         self.addr = addr
         self.timestamp = time.time()
         self.ra = RequestAuthority.RequestAuthority()
         self.stage = 0
-        self.dh_pri_key = 0
         self.dh_secret = 0
 
     def get_response(self, message):
@@ -42,9 +41,9 @@ class Authentication():
                 dec_dh_pub_client, addr, n1 = self.cryto_service.rsa_decrypt(enc_client_msg).split(",")  # TODO decryption, get N1, public key, and verify addr,
                 print("Seen DH public key: {}, Address: {}, n1: {}".format(dec_dh_pub_client, addr, n1))
                 if addr == "{}:{}".format(self.addr[0], self.addr[1]) and False: # TODO for testing
-                    self.dh_pri_key = self.cryto_service.get_dh_pri_key()
-                    dh_pub_server = self.cryto_service.get_dh_pub_key(self.dh_pri_key)
-                    secret = self.cryto_service.get_dh_secret(self.dh_pri_key, dec_dh_pub_client)
+                    dh_pri_key = self.cryto_service.get_dh_pri_key()
+                    dh_pub_server = self.cryto_service.get_dh_pub_key(dh_pri_key)
+                    secret = self.cryto_service.get_dh_secret(dh_pri_key, dec_dh_pub_client)
                     # compose response: public key, K{N1, salt}, sign whole message
                     self.stage = 2
                     return "{}, {}, salt, sign whole message".format(dh_pub_server, n1)
